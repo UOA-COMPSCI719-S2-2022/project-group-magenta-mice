c2adf8f8ad35cf07e83255fb8616af021c27d8d1
bellsye <bellsyeillman@gmail.com>
2022-11-02 19:00:06 +1300

    added tagging and search system

--- routes/application-routes.js
+++ routes/application-routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const upload = require("../middleware/multer-uploader.js");
 const fs = require("fs");
+//const tinymce = require("tinymce");
 
 const articlesDao = require("../modules/articles-dao.js");
 const { verifyAuthenticated } = require("../middleware/auth-middleware.js");
@@ -42,7 +43,8 @@ router.post("/submit-article", verifyAuthenticated, async function (req, res) {
 
     const user = res.locals.user;
 
-    await articlesDao.createArticle(req.body.title, req.body.content, user.id);
+    await articlesDao.createArticle(req.body.title, req.body.content, user.id, req.body.tags);
+
     res.setToastMessage("Article posted!");
     res.redirect("/my-articles");
 
@@ -55,6 +57,39 @@ router.post("/delete-article", verifyAuthenticated, async function(req, res) {
     await articlesDao.deleteArticle(req.body.articleId);
     res.setToastMessage("Article Deleted!");
     res.redirect("/my-articles");
+
+});
+
+//Whenever we navigate to /edit-article, verify that we're authenticated. If we are, render the edit article editor.
+// WORKING ON THIS
+/*router.post("/edit-article", verifyAuthenticated, async function(req, res) {
+
+    res.locals.title = "Edit Article";
+
+    const article = await articlesDao.retrieveArticleBy(req.body.articleId);
+    //const title = await articlesDao.retrieveArticleBy(req.body.title);
+    res.locals.title = req.body.title;
+    res.locals.article = article;
+    console.log(article);
+
+
+    res.render("article-editor-duplicate");
+});*/
+
+// Whenever we navigate to /search-articles, search for the articles with relevant tags and render the search view
+router.post("/search-articles", async function (req, res) {
+
+    const articleSearch = req.body.articleSearch;
+    res.locals.articleSearch = articleSearch;
+
+    const articles  = await articlesDao.searchArticlesBy(req.body.articleSearch);
+    res.locals.articles = articles;
+    
+    res.locals.title = `Results for ${articleSearch}`; //at the moment searches for exact matches in tags, could change to search for close matches too?
+
+    console.log(articles);
+
+    res.render("article-search");
 
 });
 
@@ -78,20 +113,5 @@ router.post("/rating", verifyAuthenticated, async function (req, res) {
 
 });
 
-//Whenever we navigate to /edit-article, verify that we're authenticated. If we are, render the edit article editor.
-// WORKING ON THIS
-router.post("/edit-article", verifyAuthenticated, async function(req, res) {
-
-    //res.locals.title = "Edit Article";
-
-    const article = await articlesDao.retrieveArticle(req.body.articleId);
-    const title = await articlesDao.retrieveArticle(req.body.title);
-    res.locals.title = req.body.title;
-    res.locals.article = article;
-    console.log(article);
-
-
-    res.render("article-editor-duplicate");
-});
 
 module.exports = router;
