d2113b896b7a354ddb430b0f3ca103c2bf53c9ea
Ying-Ni Huang <yhua308@aucklanduni.ac.nz>
2022-11-04 20:03:12 +1300

    1 final

--- middleware/comments.js
+++
@@ -1,9 +0,0 @@
-const { default: mongoose, Schema } = require("mongoose");
-
-const Comment=  mongoose.model ("Comment", {
-    comment_content: String,
-    userId: {type: Schema.Types.ObjectId, ref: 'comment'}
-});
-
-module.exports = Comment
-
